f647cb454b22c1fd6c1491684ea132dd112f644a
Iyiola <48067557+ialoko@users.noreply.github.com>
2022-06-28 03:26:35 -0400

    changed settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 import dj_database_url
+from decouple import config
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,7 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$1lhn_#-kv4j7#fcvk+%bg(7(6z*+^e^1^i+1ml$x=74t_w@@8'
+
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
